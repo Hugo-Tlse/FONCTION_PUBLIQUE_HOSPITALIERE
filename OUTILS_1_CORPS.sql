ecafe9a4ef5cfb1e1775f41a907b1774b4ed7a5a
Hugo_Tlse <hugodu31500@gmail.com>
2026-08-07 23:15:07 +0200

    Create OUTILS_1_CORPS.sql

---
+++ OUTILS_1_CORPS.sql
@@ -0,0 +1,83 @@
+
+
+
+INSERT INTO `OUTILS_1_CORPS` (`Corps`, `Grade`, `code`) VALUES
+('Aide-soignant', 'Classe normale', 'AS_CN_1'),
+('Aide-soignant', 'Classe supérieure', 'AS_CS_2'),
+('Aide-soignant', 'Principal', 'AS_PR_3'),
+
+('Auxiliaire de puériculture', 'Classe normale', 'AP_CN_1'),
+('Auxiliaire de puériculture', 'Classe supérieure', 'AP_CS_2'),
+
+('Agent des services hospitaliers qualifié', 'Classe normale', 'ASHQ_CN_1'),
+('Agent des services hospitaliers qualifié', 'Classe supérieure', 'ASHQ_CS_2'),
+
+('Agent d''entretien qualifié', 'Classe normale', 'AEQ_CN_1'),
+
+('Agent des services mortuaires et de désinfection', '1re catégorie', 'ASMD_C1_1'),
+
+('Agent de maîtrise', 'Classe normale', 'AM_CN_1'),
+('Agent de maîtrise', 'Principal', 'AM_PR_2'),
+
+('Adjoint administratif hospitalier', 'Adjoint administratif', 'AAH_CN_1'),
+('Adjoint administratif hospitalier', 'Adjoint administratif principal de 2e classe', 'AAH_P2_2'),
+('Adjoint administratif hospitalier', 'Adjoint administratif principal de 1re classe', 'AAH_P1_3'),
+
+('Adjoint des cadres hospitaliers', 'Classe normale', 'ACH_CN_1'),
+('Adjoint des cadres hospitaliers', 'Classe supérieure', 'ACH_CS_2'),
+('Adjoint des cadres hospitaliers', 'Classe exceptionnelle', 'ACH_CE_3'),
+
+('Assistant médico-administratif', 'Classe normale', 'AMA_CN_1'),
+('Assistant médico-administratif', 'Classe supérieure', 'AMA_CS_2'),
+('Assistant médico-administratif', 'Classe exceptionnelle', 'AMA_CE_3'),
+
+('Assistant médico-administratif (Assistant de régulation médicale)', 'Classe supérieure', 'AMA_ARM_CS_1'),
+('Assistant médico-administratif (Assistant de régulation médicale)', 'Classe exceptionnelle', 'AMA_ARM_CE_2'),
+
+('Animateur', 'Principal de 1re classe', 'ANIM_P1_1'),
+
+('Ambulancier', 'Classe normale', 'AMB_CN_1'),
+('Ambulancier', 'Principal', 'AMB_PR_2'),
+
+('Assistant de service social', 'Grade 2', 'ASS_G2_2'),
+
+('Assistant socio-éducatif - Éducateur spécialisé', 'Grade 1', 'ASE_ES_G1_1'),
+('Assistant socio-éducatif - Éducateur spécialisé', 'Grade 2', 'ASE_ES_G2_2'),
+
+('Conseiller en économie sociale et familiale', 'Grade 1', 'CESF_G1_1'),
+('Conseiller en économie sociale et familiale', 'Grade 2', 'CESF_G2_2'),
+
+('Éducateur de jeunes enfants', 'Grade 1', 'EJE_G1_1'),
+('Éducateur de jeunes enfants', 'Grade 2', 'EJE_G2_2'),
+
+('Éducateur technique spécialisé', 'Grade 1', 'ETS_G1_1'),
+
+('Attaché d''administration hospitalière', 'Attaché', 'AAH_ATT_CN_1'),
+
+('Chargé de mission', 'Classe 3', 'CM_C3_1'),
+('Chargé de mission', 'Classe unique', 'CM_CN_1'),
+
+('Coordinateur de recherche clinique', 'Niveau 3', 'CRC_N3_1'),
+
+('Attaché de recherche clinique', 'Niveau 2', 'ARC_N2_1'),
+
+('Chef de projet recherche clinique', 'Niveau 4', 'CPR_N4_1'),
+('Chef de projet recherche clinique', 'Niveau clinique', 'CPR_CLI_2'),
+
+('Psychologue', 'Classe normale', 'PSY_CN_1'),
+('Psychologue', 'Hors classe', 'PSY_HC_2'),
+
+('Physicien médical', 'Classe unique', 'PHYS_CN_1'),
+
+('Audioprothésiste', 'Classe unique', 'AUD_CN_1'),
+
+('Diététicien', 'Classe supérieure', 'DIET_CS_1'),
+('Diététicien', 'Cadre de santé', 'DIET_CDS_2'),
+
+('Ergothérapeute', 'Cadre de santé', 'ERGO_CDS_1'),
+
+('Orthophoniste', 'Classe supérieure', 'ORTHO_CS_1'),
+
+('Orthoptiste', 'Classe supérieure', 'ORTPT_CS_1'),
+
+('Pédicure-podologue', 'Classe normale', 'PEDPOD_CN_1');
